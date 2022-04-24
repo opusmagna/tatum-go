e064e37a979773818a82d8005582e77a1593f2d8
opusmagna <eylem.avcin@yandex.com>
2022-04-24 23:52:09 +0300

    Add offchain/account -> onward

---
+++ offchain/account.go
@@ -0,0 +1,31 @@
+package offchain
+
+import (
+	"encoding/json"
+	"fmt"
+
+	r "github.com/opusmagna/tatum-go/model/response/offchain"
+)
+
+/*
+ */
+func CreateNewDepositAddress(accountId string) (*r.Address, error) {
+
+	_url := fmt.Sprintf("/v3/offchain/%s/broadcast", accountId)
+
+	var res string
+	var body []byte
+	var responseAddress r.Address
+	res, err := sender.SendPost(_url, body)
+	if err == nil {
+		err = json.Unmarshal([]byte(res), &responseAddress)
+	}
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	return &responseAddress, nil
+
+}
